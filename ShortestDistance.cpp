78bcfbba62ebe8620fdd03872109e0fbce91ea46
md-pranta <juhurenurlhaque@gmail.com>
2023-09-01 16:21:10 +0600

    solve shortestDistance

--- ShortestDistance.cpp
+++ ShortestDistance.cpp
@@ -1,63 +1,78 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
-#define SetBit(x, k) (x |= (1LL << k))
-#define ClearBit(x, k) (x &= ~(1LL << k))
-#define CheckBit(x, k) ((x>>k)&1)
-#define M 10000000007
-typedef pair<ll, int>pii;
-
 const ll INF = numeric_limits<ll>::max();
 const int N = 100006;
 
-int main() {
+int main(){
     int n, e;
     cin >> n >> e;
-
-    vector<vector<pair<int, long long>>> graph(n + 1);
-    for (int i = 0; i < e; ++i) {
-        int u, v;
-        long long w;
-        cin >> u >> v >> w;
-        graph[u].emplace_back(v, w);
+    ll dis[n + 1][n + 1];
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            dis[i][j] = INF;
+            if (i == j)
+                dis[i][j] = 0;
+        }
     }
+    while (e--)
+    {
+        int a, b;
+        ll w;
+        cin >> a >> b >> w;
+        if(dis[a][b] <= w)continue;
+        dis[a][b] = w;
+    }
+    // for (int i = 1; i <= n; i++){
+    //     for (int j = 1; j <= n; j++){
+    //         if (dis[i][j] == INF)
+    //             cout << "INF"
+    //                  << " ";
+    //         else
+    //             cout << dis[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
 
-    int q;
-    cin >> q;
-    while(q--) {
-        int x, y;
-        cin >> x >> y;
-
-        vector<long long> distance(n + 1, INF);
-        priority_queue<pii, vector<pii>, greater<>> pq;
-
-        distance[x] = 0;
-        pq.push({0, x});
-
-        while (!pq.empty()) {
-            int u = pq.top().second;
-            long long dist = pq.top().first;
-            pq.pop();
-
-            if (dist > distance[u]) continue;
-
-            for (auto vpii : graph[u]) {
-                int v = vpii.first;
-                ll w = vpii.second;
-                if (distance[u] + w < distance[v]) {
-                    distance[v] = distance[u] + w;
-                    pq.push({distance[v], v});
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++){
+                if (dis[i][k] != INF && dis[k][j] != INF) {
+                    dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
                 }
             }
         }
-
-        if (distance[y] == INF) {
-            cout << -1 << endl;
-        } else {
-            cout << distance[y] << endl;
-        }
     }
-
+    int q;
+    cin >> q;
+    while(q--){
+        int x, y;
+        cin >> x >> y;
+        if (dis[x][y] == INF)
+            cout <<-1<<endl;
+        else
+            cout << dis[x][y] <<endl;
+    }
     return 0;
 }
+/*
+4 7
+1 2 10
+2 3 5
+3 4 2
+4 2 3
+3 1 7
+2 1 1
+1 4 4
+6
+1 2
+4 1
+3 1
+1 4
+2 4
+4 2
+*/
